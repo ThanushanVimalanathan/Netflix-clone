9977f5e58d90134070dcbb97db95077cbbd1b498
VimalanathanThanushan <thanushaan69@gmail.com>
2025-12-18 17:30:16 +0530

    Feat:Completed Sign in / Login Form

--- Netflix-clone/src/App.jsx
+++ Netflix-clone/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Home from './Pages/Home/Home'
+import Login from './Pages/Login/Login'
+import Player from './Pages/Player/Player'
+import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+      
     </div>
   )
 }
