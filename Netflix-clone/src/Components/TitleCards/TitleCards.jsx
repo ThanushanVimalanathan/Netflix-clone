16b2844fcd9484e9d0963f4177733dc7ac16c20a
VimalanathanThanushan <thanushaan69@gmail.com>
2025-12-18 23:47:57 +0530

    Feat:fetch movie API

--- Netflix-clone/src/Components/TitleCards/TitleCards.jsx
+++ Netflix-clone/src/Components/TitleCards/TitleCards.jsx
@@ -1,20 +1,37 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title,category}) => {
 
-
+  const [apiData, setApiData] = useState([]);
 
 {/* function for scroll Y axis if movie card using useref and useEffect */}
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3M2Y5MmI1M2U0YzQxMTI5YmQ3Y2UwZjAwZGIzOTY1YiIsIm5iZiI6MTc2NjA3NjU1MC42MjYsInN1YiI6IjY5NDQzMDg2YThiM2VmYTllOTM4YmViZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DjRKlLfa4mlmzANMPD8YRmqZebDof8PZdv4KGelr17E'
+   }
+ };
+
+ 
+
+
   const handleWheel = (event)=>{
       event.preventDefault();
       cardsRef.current.scrollLeft += event.deltaY;
   }
 
   useEffect(()=>{
-        cardsRef.current.addEventListener('wheel',handleWheel)
+         fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+         .then(res => res.json())
+         .then(res => setApiData(res.results))
+         .catch(err => console.error(err));
+
+          cardsRef.current.addEventListener('wheel',handleWheel)
   },[])
 
   return (
@@ -22,10 +39,10 @@ const TitleCards = ({title,category}) => {
         <h1>{title ? title : "Popular on Netflix"}</h1>
         <div className='card-list' ref={cardsRef}>
           {/* mount card data here */}
-          {cards_data.map((card,index)=>{
+          {apiData.map((card,index)=>{
             return <div className='card' key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+              <p>{card.original_title}</p>
             </div>
 
           })}
--- Netflix-clone/src/Pages/Home/Home.jsx
+++ Netflix-clone/src/Pages/Home/Home.jsx
@@ -28,10 +28,10 @@ const Home = () => {
           </div>
         </div>
         <div className='more-cards'>
-          <TitleCards title={"Blockbuster Movies"} />
-          <TitleCards title={"Only on Netflix"} />
-          <TitleCards title={"Upcomming"} />
-          <TitleCards title={"Top Pics for You"} />
+          <TitleCards title={"Blockbuster Movies"} category={"top_rated"} />
+          <TitleCards title={"Only on Netflix"} category={"popular"} />
+          <TitleCards title={"Upcomming"} category={"upcoming"} />
+          <TitleCards title={"Top Pics for You"} category={"now_playing"} />
 
         </div>
         <Footer />
