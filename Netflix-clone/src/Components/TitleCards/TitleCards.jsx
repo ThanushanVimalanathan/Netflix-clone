c9f2e11eb0332333fc683caa04554cb1b64d81c8
VimalanathanThanushan <thanushaan69@gmail.com>
2025-12-18 16:01:15 +0530

    Feat:Completed home page

--- Netflix-clone/src/Components/TitleCards/TitleCards.jsx
+++ Netflix-clone/src/Components/TitleCards/TitleCards.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+const TitleCards = ({title,category}) => {
+
+
+
+{/* function for scroll Y axis if movie card using useref and useEffect */}
+  const cardsRef = useRef();
+  const handleWheel = (event)=>{
+      event.preventDefault();
+      cardsRef.current.scrollLeft += event.deltaY;
+  }
+
+  useEffect(()=>{
+        cardsRef.current.addEventListener('wheel',handleWheel)
+  },[])
+
   return (
-    <div className='titlecards'>
+    <div className='title-cards'>
+        <h1>{title ? title : "Popular on Netflix"}</h1>
+        <div className='card-list' ref={cardsRef}>
+          {/* mount card data here */}
+          {cards_data.map((card,index)=>{
+            return <div className='card' key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+
+          })}
+        </div>
       
     </div>
   )
